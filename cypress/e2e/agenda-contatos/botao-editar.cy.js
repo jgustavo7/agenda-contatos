4f5d83d09bb22c5761fa7486120842c36c8dd26e
jgustavo <jgustavo0711@gmail.com>
2025-01-06 16:19:31 -0300

    testes alterados e finalizados com sucesso

--- cypress/e2e/agenda-contatos/botao-editar.cy.js
+++ cypress/e2e/agenda-contatos/botao-editar.cy.js
@@ -6,6 +6,13 @@ describe('Testes para botões deletar e editar', () => {
     })
 
     it('Deve editar os contatos', () => {
-        cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()  
+        cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
+        cy.get('[type="text"]').clear()
+        cy.get('[type="text"]').type('Dailane Rodrigues')
+        cy.get('[type="email"]').clear()
+        cy.get('[type="email"]').type('dailane@teste.com')
+        cy.get('[type="tel"]').clear()
+        cy.get('[type="tel"]').type('11 123456789')
+        cy.get('.alterar').click()
     })
 })
